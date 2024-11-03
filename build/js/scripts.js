06214c702c62526b56b0bb5c062c7854b200f9ef
Yakup <yakupjoraev@gmail.com>
2024-11-04 01:48:20 +0300

    added new pages

---
+++ build/js/scripts.js
@@ -0,0 +1,131 @@
+// Custom Scripts
+// Custom scripts
+// Мобильное меню бургер
+function burgerMenu() {
+  const burger = document.querySelector('.burger')
+  const menu = document.querySelector('.nav')
+  const body = document.querySelector('body')
+  burger.addEventListener('click', () => {
+    if (!menu.classList.contains('active')) {
+      menu.classList.add('active')
+      burger.classList.add('active-burger')
+      body.classList.add('locked')
+    } else {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+  //снять классы при клике на элементы меню
+  const menuItems = document.querySelectorAll('.nav__item')
+
+  menuItems.forEach(item => {
+    item.addEventListener('click', function () {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    })
+  });
+
+  // Вот тут мы ставим брейкпоинт навбара
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 991.98) {
+      menu.classList.remove('active')
+      burger.classList.remove('active-burger')
+      body.classList.remove('locked')
+    }
+  })
+}
+burgerMenu();
+
+
+
+
+// Аккордеон
+const accordionItems = document.querySelectorAll('[data-accordion-item]');
+let openAccordion = null; // переменная для хранения ссылки на открытый аккордеон
+
+function toggleAccordion() {
+  if (openAccordion && openAccordion !== this) {
+    // Если есть открытый аккордеон, который не совпадает с текущим
+    openAccordion.classList.remove('active'); // закрыть его
+    const openAccordionContent = openAccordion.nextElementSibling;
+    if (openAccordionContent) {
+      // если у аккордеона есть содержимое
+      openAccordionContent.style.maxHeight = null; // сбросить высоту контента
+    }
+  }
+
+  this.classList.toggle('active'); // открыть или закрыть текущий аккордеон
+
+  const content = this.nextElementSibling;
+  if (content) {
+    // если у аккордеона есть содержимое
+    if (content.style.maxHeight) {
+      // Если контент открыт, закрыть его
+      content.style.maxHeight = null;
+    } else {
+      // Если контент закрыт, открыть его
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  }
+
+  openAccordion = this; // запомнить ссылку на открытый аккордеон
+}
+
+
+accordionItems.forEach(item => item.addEventListener('click', toggleAccordion));
+
+function specialTabs() {
+  const container = document.querySelector('.tab');
+
+  if (!container) {
+    return null
+  }
+
+  const tabButtons = document.querySelectorAll('.tab__btn');
+  const tabContents = document.querySelectorAll('.tab__content');
+
+  tabButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const target = button.getAttribute('data-tab-btn');
+
+      // Удаляем класс active у всех кнопок и контента
+      tabButtons.forEach(btn => btn.classList.remove('active'));
+      tabContents.forEach(content => content.classList.remove('active'));
+
+      // Добавляем класс active к выбранной кнопке и соответствующему контенту
+      button.classList.add('active');
+      document.querySelector(`.tab__content[data-tab-content="${target}"]`).classList.add('active');
+    });
+  });
+}
+
+specialTabs();
+
+
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+const modals = document.querySelectorAll('.modal');
+
+openModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+  });
+});
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal.classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    event.target.classList.remove('show');
+  }
+});
+
